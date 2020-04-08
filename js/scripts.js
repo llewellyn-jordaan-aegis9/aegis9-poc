cf44bbb8cde7289c7313cf52503ff94e0bf9b670
Llewellyn Jordaan <Llewellyn.Jordaan@aegis9.com.au>
2020-04-09 08:56:42 +1000

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,75 @@
 import $ from 'jquery';
 
+/* eslint-disable */
+
+// TYPE WRITER
+// values to keep track of the number of letters typed, which quote to use. etc. Don't change these values.
+var i = 0,
+    a = 0,
+    isBackspacing = false,
+    isParagraph = false;
+
+
+// Speed (in milliseconds) of typing.
+var speedForward = 120, //Typing Speed
+    speedWait = 1000, // Wait between typing and backspacing
+    speedBetweenLines = 1000, //Wait between first and second lines
+    speedBackspace = 70; //Backspace Speed
+
+ 
+function typeWriter(id, ar) {
+    var element = $("#" + id),
+    aString = ar[a],
+    eHeader = element.children("span"), //Header element
+    eParagraph = element.children("p"); //Subheader element
+
+
+    // Determine if animation should be typing or backspacing
+    if (!isBackspacing) {
+        
+        if (i < aString.length) {
+            if (!isParagraph) {
+                eHeader.text(eHeader.text() + aString.charAt(i));
+            } else {
+                eParagraph.text(eParagraph.text() + aString.charAt(i));
+            }
+
+            i++;
+
+            setTimeout(function(){ typeWriter(id, ar); }, speedForward);
+        } else if (i == aString.length) {
+            isBackspacing = true;
+
+            setTimeout(function(){ typeWriter(id, ar); }, speedWait);
+        }
+        
+    // If backspacing is enabled
+    } else {
+        // If either the header or the paragraph still has text, continue backspacing
+        if (eHeader.text().length > 0 || eParagraph.text().length > 0) {
+        
+        // If paragraph still has text, continue erasing, otherwise switch to the header.
+        if (eParagraph.text().length > 0) {
+            eParagraph.text(eParagraph.text().substring(0, eParagraph.text().length - 1));
+        } else if (eHeader.text().length > 0) {
+            eParagraph.removeClass("cursor");
+            eHeader.addClass("cursor");
+            eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));
+        }
+        setTimeout(function(){ typeWriter(id, ar); }, speedBackspace);
+        
+        // If neither head or paragraph still has text, switch to next quote in array and start typing.
+        } else { 
+        
+        isBackspacing = false;
+        i = 0;
+        isParagraph = false;
+        a = (a + 1) % ar.length; //Moves to next position in array, always looping back to 0
+        setTimeout(function(){ typeWriter(id, ar); }, 50);
+        
+        }
+    }
+}
 
 $(document).ready(function () {
     "use strict";
@@ -18,7 +88,18 @@ $(document).ready(function () {
 
     }
 
-          
+    var textArray = [
+        "Service Providers",
+        "Organisations",
+        "Small Businesses",
+        "Government Agencies",
+        "Private Enterprise",
+        "Industry Leaders"
+    ];
+
+     //Run the loop
+    typeWriter("type-output", textArray);
+     
     // Mobile Menu
     /*
     $('.mobile-toggle').click(function() {
