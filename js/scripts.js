6608b2d111dded8111e2549a4a0ce1d54e109d84
Llewellyn Jordaan <Llewellyn.Jordaan@aegis9.com.au>
2020-04-23 09:58:24 +1000

    Updates
    Moved services text blob to services section. Aligned methods page text. Removed BSS section from methods.
    Standardised banners

--- js/scripts.js
+++ js/scripts.js
@@ -76,15 +76,7 @@ function typeWriter(id, ar) {
 $(document).ready(function () {
     "use strict";
 
-    $( ".sub-nav-item" ).click(function() {
-        $('.sub-nav-item').removeClass('active-sub-nav-item');
-
-        $(this).addClass('active-sub-nav-item');
-
-        $('html,body').animate({
-            scrollTop: $('.secondary-nav').offset().top - 60
-        }, 'slow');
-    });
+    
 
     // Smooth scroll to inner links
     var innerLinks = $('a.inner-link');
